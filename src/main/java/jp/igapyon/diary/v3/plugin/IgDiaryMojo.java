a6f61c2e75877603320e2151d53878305c39a492
Toshiki Iga <igapyon@gmail.com>
2017-01-29 23:40:38 +0900

    save

---
+++ src/main/java/jp/igapyon/diary/v3/plugin/IgDiaryMojo.java
@@ -0,0 +1,62 @@
+/*
+ *  Igapyon Diary system v3 (IgapyonV3).
+ *  Copyright (C) 2015-2017  Toshiki Iga
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ *  Copyright 2015-2017 Toshiki Iga
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package jp.igapyon.diary.v3.plugin;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import jp.igapyon.diary.v3.IgDiaryProcessor;
+
+/**
+ * mvn jp.igapyon.diary.v3.plugin:igdiary-maven-plugin:1.0:igdiary
+ */
+@Mojo(name = "igdiary")
+public class IgDiaryMojo extends AbstractMojo {
+	public void execute() throws MojoExecutionException {
+		try {
+			// カレントディレクトリを取得のうえ正規化します。
+			final File rootdir = new File(".").getCanonicalFile();
+
+			// 基本処理。
+			new IgDiaryProcessor().process(rootdir);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new MojoExecutionException("Error processing: ", e);
+		}
+	}
+}
